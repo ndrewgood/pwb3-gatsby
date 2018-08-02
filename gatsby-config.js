5150396f84d8e82c93d591dff7492979cac40193
Andrew G <gandrew888@gmail.com>
2018-08-02 19:38:06 -0400

    added Google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,5 +32,18 @@ module.exports = {
                 }
               }
     }
+    {
+  resolve: `gatsby-plugin-google-analytics`,
+  options: {
+    trackingId: "UA-123340572-1",
+    // Puts tracking script in the head instead of the body
+    head: true,
+    // Setting this parameter is optional
+    anonymize: true,
+    // Setting this parameter is also optional
+    respectDNT: true,
+    // Avoids sending pageview hits from custom paths
+  },
+},
   ],
 }
